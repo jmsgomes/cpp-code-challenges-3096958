63d2e2edeaeb22b38a24205b3e78bdec29a6b323
jmsgomes <3680160+jmsgomes@users.noreply.github.com>
2024-04-17 16:42:37 +0000

    Complete Challenge #1: Checking for Palindromes

--- src/01/Challenge/ch1_palindrome.cpp
+++ src/01/Challenge/ch1_palindrome.cpp
@@ -4,26 +4,32 @@
 // Write a function to check if a string is a palindrome or not.
 // Examples: civic, radar, level.
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 // is_palindrome()
-// Summary: This function receives a string and returns true if the string is a palindrome, false otherwise.
-// Arguments:
+// Summary: This function receives a string and returns true if the string is a
+// palindrome, false otherwise. Arguments:
 //           str: The string to analyze.
 // Returns: A boolean value. True for palindromes, false otherwise.
-bool is_palindrome(std::string str){
-
-    // Write your code here
-
-    return false;
+bool is_palindrome(std::string str) {
+  int length = str.length();
+  int last = length - 1;
+  int midpoint = length / 2;
+  for (int i = 0; i < midpoint; i++) {
+    if (str[i] != str[last - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // Main function
-int main(){
-    std::string s;
-    std::cout << "Enter a string: " << std::flush;
-    std::getline(std::cin,s);
-    std::cout << "\n\"" << s << (is_palindrome(s) ? "\" is" : "\" is not" ) << " a palindrome.\n\n";
-    return 0;
+int main() {
+  std::string s;
+  std::cout << "Enter a string: " << std::flush;
+  std::getline(std::cin, s);
+  std::cout << "\n\"" << s << (is_palindrome(s) ? "\" is" : "\" is not")
+            << " a palindrome.\n\n";
+  return 0;
 }
